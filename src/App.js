597e1846eedf586829d1844d67cd31987c3bd408
moneeto <joako600@hotmail.com>
2023-03-20 15:05:28 -0300

    Added timer with custom hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import './stylesheets/boton.css'
+import './stylesheets/contador.css'
+import Boton from './componentes/Boton';
+import logoNike from './imagenes/nike-logo.png';
+import Contador from './componentes/Contador';
+import { useState } from 'react';
+import Tiempo from './componentes/Tiempo';
+import { useTiempo } from "./hooks/useTiempo";
+import './stylesheets/tiempo.css'
 
 function App() {
+  
+
+
+
+
+  const [segundos, setConteo] = useTiempo(10);
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+    setConteo()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='logo-contenedor'>
+        <img 
+        className='logo'
+        src={logoNike}
+        alt='Logo del contador de clicks'
+        />
+      </div>
+      <div className='contenedor-contador'>
+        <h1 className='titulo'>Contador de Clicks</h1>
+        <Contador 
+        numClics={numClics}
+        />
+        <Tiempo
+        segundos={segundos}
+        setConteo={setConteo}
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Boton 
+        texto = 'Click'
+        esBotonDeClic={true}
+        manejarClic={manejarClic}
+        segundos={segundos}/>
+        <Boton 
+        texto = 'Reiniciar'
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador}/>
+      </div>
     </div>
   );
-}
+  }
 
 export default App;
